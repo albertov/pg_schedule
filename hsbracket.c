3d4ed9a6305b7a206f3c234a0e5acf4afa93cbc5
Alberto Valverde <alberto@toscat.net>
2018-03-13 15:16:08 +0100

    hs_add_root is deprecated in ghc 8.4.1

--- hsbracket.c
+++ hsbracket.c
@@ -2,8 +2,6 @@
 #include "Rts.h"
 #include <stdio.h>
 
-extern void __stginit_PGSchedule ( void );
-
 static void pg_schedule_enter(void) __attribute__((constructor));
 static void pg_schedule_enter(void)
 {
@@ -12,7 +10,6 @@ static void pg_schedule_enter(void)
   RtsConfig conf = defaultRtsConfig;
   conf.rts_opts_enabled = RtsOptsAll;
   hs_init_ghc(&argc, &argv_, conf);
-  hs_add_root(__stginit_PGSchedule);
 }
 
 static void pg_schedule_exit(void) __attribute__((destructor));
